ec94693c6657cdd9b88fd2c1f7453e3ed836ed48
mariechow-oo <117883627+mariechow-oo@users.noreply.github.com>
2022-11-22 16:04:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-// Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
-var name = 'hello';
-// TODO write your code here...
+<!DOCTYPE html>
+<html>
+
+<head>
+
+</head>
+
+<body>
+  <!-- <script type="javascript" src="./test.js"> -->
+  <script>
+    // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
+    var name = 'hello';
+    name.toUpperCase();
 
 
-// Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
-var sentence = 'good afternoon, mr mike.';
-//TODO write your code here...
+    // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
+    var sentence = 'good afternoon, mr mike.';
+    sentence = sentence.split(' ').map((word) => word.charAt(0).toUpperCase() + word.substring(1)).join(' ');
 
+    // Complete the following procedure to extract the amount of money variable, output: 20.
+    var money = '￥20';
+    money = money.replace("￥", "");
 
-// Complete the following procedure to extract the amount of money variable, output: 20.
-var money = '￥20';
-//TODO write your code here...
+  </script>
+  test page
+</body>
+
+</html>
